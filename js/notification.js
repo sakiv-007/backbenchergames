c5bc0abd81f438e92c5797881e922fe4018b442e
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-04-05 15:21:53 +0530

    chore: update notification version and fix typo in content
    Update the DEFAULT_NOTIFICATION_VERSION to '2.9' to reflect changes in the default notification. Correct a capitalization typo in the notification content for consistency.

--- js/notification.js
+++ js/notification.js
@@ -5,7 +5,7 @@ let notifications = [];
 let unseenCount = 0;
 
 // Version control for default notification
-const DEFAULT_NOTIFICATION_VERSION = '2.8'; // Increment this when default notification changes
+const DEFAULT_NOTIFICATION_VERSION = '2.9'; // Increment this when default notification changes
 
 // Check if we have stored notifications in localStorage
 function loadNotifications() {
@@ -19,7 +19,7 @@ function loadNotifications() {
     const title = 'New Feature Alert!';
 
     const content = `We Updated and made the hero section game fully functional <br> <br>
-    and we also added the feature to edit your profile that will store your data locally`;
+    And we also added the feature to edit your profile that will store your data locally`;
 
     const date = 'Released: April 5, 2025';
 

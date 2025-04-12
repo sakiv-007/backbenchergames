8876ae25676beefe86fc612348b7443635fd4bf4
Vikas Kumar Shah <vikas.kmr.vk15@gmail.com>
2025-04-12 18:38:15 +0530

    docs(notification): update default notification content and version
    Update the content of the default notification to reflect new features and improvements. Increment the `DEFAULT_NOTIFICATION_VERSION` to '2.4' to ensure users receive the updated notification. Changes include adding details about the new Rock Paper Scissors game, Word-search game availability, UI improvements, hero section functionality, and profile editing feature.

--- js/notification.js
+++ js/notification.js
@@ -5,7 +5,7 @@ let notifications = [];
 let unseenCount = 0;
 
 // Version control for default notification
-const DEFAULT_NOTIFICATION_VERSION = '2.2'; // Increment this when default notification changes
+const DEFAULT_NOTIFICATION_VERSION = '2.4'; // Increment this when default notification changes
 
 // Check if we have stored notifications in localStorage
 function loadNotifications() {
@@ -19,15 +19,26 @@ function loadNotifications() {
     const title = 'New Feature Alert!';
 
     const content = `
-    Added a new Game - Rock Paper Scissors
+
+    Added a new Game - Rock Paper Scissors. Try it out now!
 
     <br> <br>
 
-    Word-search game is now available<br><br>Made some UI improvements<br><br>We Updated and made the hero section game fully functional
+    Word-search game is now available
+    
+    <br><br>
+    
+    Made some UI improvements
+    
+    <br><br>
+    
+    We Updated and made the hero section game fully functional
 
     <br> <br>
 
-    And we also added the feature to edit your profile that will store your data locally`;
+    And we also added the feature to edit your profile that will store your data locally
+    
+    `;
 
     const date = 'Released: April 12, 2025';
 
